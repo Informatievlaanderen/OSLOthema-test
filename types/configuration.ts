6f32c58e25678ed36b07674ee17ed5618a06226c
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-11-20 14:27:46 +0100

    Initialize config

---
+++ types/configuration.ts
@@ -0,0 +1,27 @@
+export interface Configuration {
+  title: string;
+  category: string;
+  usage: string;
+  status: string;
+  responsibleOrganisation: Document[];
+  publicationDate: string;
+  descriptionFileName: string;
+  specificationDocuments: Document[];
+  documentation: Document[];
+  charter: Document;
+  reports: Document[];
+  presentations: Document[];
+  dateOfRegistration: string;
+  dateOfAcknowledgementByWorkingGroup: string;
+  dateOfAcknowledgementBySteeringCommittee: string;
+  datePublicReviewStart: string;
+  datePublicReviewEnd: string;
+  endOfPublicationDate: string;
+  domain: string;
+  functionalScope: string;
+}
+
+interface Document {
+  name?: string;
+  resourceReference?: string;
+}
